56dcc2ec91800de4a8a8a745283a969c4f2401a1
FabriqueDWWM <71813538+FabriqueDWWM@users.noreply.github.com>
2022-01-11 19:19:54 +0100

    Update ex16.js

--- JS-Algo/exercices_entrainement/ex16/js/ex16.js
+++ JS-Algo/exercices_entrainement/ex16/js/ex16.js
@@ -1,4 +1,4 @@
-var nombre = 1023;
-var nombreDeDizaines = Math.floor(nombre/10);
+const nombre = 1023;
+const nombreDeDizaines = Math.floor(nombre/10);
 
 document.writeln("Nombre de dizaines dans "+nombre+": "+nombreDeDizaines);
